e52a0e2a150814643bd91ed204fe31b90e8d2228
shahin <shahin.asadi.95@gmail.com>
2023-03-31 18:44:51 +0430

    add environment variables

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env   # Add Environment 
 
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
